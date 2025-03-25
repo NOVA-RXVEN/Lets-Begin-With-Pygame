66e8a3630244ad82c20a2d6e02236bf37447ed31
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-03-25 20:08:07 +0600

    Acp

--- Acp.py
+++ Acp.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-white = (255, 255, 255)
+beige = (238, 223, 201)
 
 clock = pygame.time.Clock()
 
@@ -10,15 +10,15 @@ display_surface = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("Image")
 
-image = pygame.image.load("HALL.jpeg")
-SIZE = (300, 300)
+image = pygame.image.load("Weeknd.png")
+SIZE = (400, 400)
 
 image = pygame.transform.scale(image, SIZE)
-POSITION = (100,100)
+POSITION = (50, 50)
 
 while True: 
     
-    display_surface.fill(white)
+    display_surface.fill(beige)
     display_surface.blit(image, POSITION)
     
     for event in pygame.event.get():
